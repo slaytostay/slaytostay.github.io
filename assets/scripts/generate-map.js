8c4a04d6533104e76cf00414d135036cf05d49ed
Slay to Stay <42868422+slaytostay@users.noreply.github.com>
2018-09-19 17:44:19 +0200

    Update generate-map.js

--- assets/scripts/generate-map.js
+++ assets/scripts/generate-map.js
@@ -58,17 +58,16 @@ function generateMap(json) {
 	var scene = document.getElementById('layers');
 	
 	var $section = $('.layers');
-          var $panzoom = $section.panzoom();
-          $panzoom.parent().on('mousewheel.focal', function( e ) {
-            e.preventDefault();
-            var delta = e.delta || e.originalEvent.wheelDelta;
-            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-            $panzoom.panzoom('zoom', zoomOut, {
-              increment: 0.1,
-              animate: true,
-              focal: e
-            });
-          });
-        })();
+	var $panzoom = $section.panzoom();
+	$panzoom.parent().on('mousewheel.focal', function( e ) {
+		e.preventDefault();
+		var delta = e.delta || e.originalEvent.wheelDelta;
+		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+		$panzoom.panzoom('zoom', zoomOut, {
+			increment: 0.1,
+			animate: true,
+			focal: e
+		});
+	});
 	//panzoom(scene)
 }
