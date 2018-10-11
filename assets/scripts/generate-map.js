4b33e003c04cb4622bc30d42126fc456ac4342e5
Slay to Stay <g.nikkels@students.uu.nl>
2018-10-11 18:50:55 +0200

    Map rework

--- assets/scripts/generate-map.js
+++ assets/scripts/generate-map.js
@@ -9,29 +9,44 @@ function generateMap(json) {
 
 	var draw = SVG('drawing').attr({
 			zoomAndPan:"magnify", contentStyleType:"text/css", preserveAspectRatio:"xMidYMid meet",
-			style:"background-color:black; overflow: hidden; display: inline; width: 100%; min-width: 100%; max-width: 100%; height: 720;"
+			style:"background-color:black; overflow: hidden; display: inline; width: 100%; min-width: 100%; max-width: 100%; min-height: 720; height: 95%;"
 	});
+
+	var defs = draw.defs();
+	var masks = defs.clip().attr({id:"masks"});
+
 
 	var layers = draw.group().attr({id:"layers", transform:"matrix(1 0 0 1 -5900 -2350)"});
 
 	var layerBg = layers.group().attr({id:"layer-background"});
 	layerBg.rect().attr({x:"0", y:"0", width:"8306", height:"4850"});
 
-	var layerMap = layers.group().attr({id:"layer-map", style:"opacity:.5"});
-	layerMap.image("../assets/maps/map.png").attr({
+	var layerMap = layers.group().attr({id:"layer-map", style:"opacity:1"});
+	var layerMapBack = layerMap.image("../assets/maps/map.png").attr({
 		id:"image-map",
-		y:"0",
-		x:"0",
-		height:"4850",
-		width:"8306",
 		style:"display:inline;opacity:1;image-rendering:pixelated",
 		preserveAspectRatio:"none"
 	});
 
+	var layerCover = layerMap.rect().attr({
+		x:0,y:0,width:8306,height:4850,
+		fill:"#330300",
+		style:"mix-blend-mode:multiply;opacity:.75",
+	});
+
+	var layerMapTop = layerMapBack.clone().attr({
+		x:0,y:0,width:8306,height:4850,
+		id:"image-map-top",
+		style:"display:inline;opacity:1;image-rendering:pixelated",
+		preserveAspectRatio:"none",
+		"clip-path":"url(#masks)"
+	})
+	layerMap.add(layerMapTop);
+
 	var layerAreas = layers.group().attr({class:"layer-areas"});
 	var labels = layerAreas.group().attr({class:"labels", fill:"white"});
-	var areas = layerAreas.group().attr({class:"areas", style:"mix-blend-mode: overlay;", fill:"white"})
-	for (var i = 0; i < Object.keys(json).length; i++) {
+	var areas = layerAreas.group().attr({class:"areas", style:"opacity:0", fill:"white"})
+	for (var i = 0; i < 1; i++) {
 
 		var area = json[i];
 		if (!area["coords"]) continue;
@@ -42,7 +57,6 @@ function generateMap(json) {
 			x:pos[0],y:pos[1]
 		});
 
-		var task = areas.group().id("task"+i);
 		for (var j = 0; j < coords.length; j+=2){
 			pos0 = coords[j].split(',');
 			pos1 = coords[j+1].split(',');
@@ -50,7 +64,8 @@ function generateMap(json) {
 			y = pos0[1];
 			w = pos1[0]-x;
 			h = pos1[1]-y;
-			task.rect().attr({x:x,y:y,width:w,height:h});
+			areas.rect().id("task"+i).attr({x:x,y:y,width:w,height:h});
+			masks.rect().id("taskmask"+i).attr({x:x,y:y,width:w,height:h});
 		}
 
 	}
