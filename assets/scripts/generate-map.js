af63787871578e2310ef0bf2a703972f244e230f
Slay to Stay <g.nikkels@students.uu.nl>
2018-10-13 15:15:19 +0200

    coords rework

--- assets/scripts/generate-map.js
+++ assets/scripts/generate-map.js
@@ -6,17 +6,24 @@ SVG.on(document, 'DOMContentLoaded', function() {
 })
 
 function generateMap(json) {
-
+	if (startId == -1) {
+		startId = 0;
+		endId = Object.keys(json).length;
+	}
 	var draw = SVG('drawing').attr({
 			zoomAndPan:"magnify", contentStyleType:"text/css", preserveAspectRatio:"xMidYMid meet",
 			style:"background-color:black; overflow: hidden; display: inline; width: 100%; min-width: 100%; max-width: 100%; min-height: 720; height: 100%;"
 	});
-
 	var defs = draw.defs();
 	var masks = defs.clip().attr({id:"masks"});
 
-
-	var layers = draw.group().attr({id:"layers", transform:"matrix(1 0 0 1 -5900 -2350)"});
+	// Needs improvement
+	var cw = draw.node.clientWidth/2;
+	var ch = draw.node.clientHeight/2;
+	var pos0 = json[startId].coords[0];
+	var sx = -(pos0.x+pos0.w/2-cw);
+	var sy = -(pos0.y+pos0.h/2+ch);
+	var layers = draw.group().attr({id:"layers", transform:"matrix(1 0 0 1 "+sx+" "+sy+")"});
 
 	var layerBg = layers.group().attr({id:"layer-background"});
 	layerBg.rect().attr({x:"0", y:"0", width:"8306", height:"4850"});
@@ -46,26 +53,27 @@ function generateMap(json) {
 	var layerAreas = layers.group().attr({class:"layer-areas"});
 	var labels = layerAreas.group().attr({class:"labels", fill:"white"});
 	var areas = layerAreas.group().attr({class:"areas", style:"opacity:0", fill:"white"})
-	for (var i = 0; i < Object.keys(json).length; i++) {
+	for (var i = startId; i < endId+1; i++) {
 
 		var area = json[i];
-		if (!area["coords"]) continue;
-		var coords = area["coords"].split(';');
+		if (!area.coords) continue;
+		var coords = area.coords;
 
-		var pos = coords[0].split(',');
+		var pos = coords[0];
 		var label = labels.plain("Task "+(i+1)+": "+area["task"]).attr({
-			x:pos[0],y:pos[1]
+			x:pos.x,y:pos.y
 		});
 
 		for (var j = 0; j < coords.length; j+=2){
-			pos0 = coords[j].split(',');
-			pos1 = coords[j+1].split(',');
-			x = pos0[0];
-			y = pos0[1];
-			w = pos1[0]-x;
-			h = pos1[1]-y;
-			areas.rect().id("task"+i).attr({x:x,y:y,width:w,height:h});
-			masks.rect().id("taskmask"+i).attr({x:x,y:y,width:w,height:h});
+			var pos = coords[j];
+			var attrs = {
+				x:pos.x,
+				y:pos.y,
+				width:pos.w,
+				height:pos.h
+			}
+			areas.rect().id("task"+i).attr(attrs);
+			masks.rect().id("taskmask"+i).attr(attrs);
 		}
 
 	}
